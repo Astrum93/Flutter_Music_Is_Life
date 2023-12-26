ff0a80d212a0d344535fb3e57795e2fbb4dd07a6
Astrum93 <jungmin2670@gmail.com>
2023-12-26 21:47:23 +0900

    user_data 생성

---
+++ lib/data/memory/user_data.dart
@@ -0,0 +1,15 @@
+class UserData {
+  String name;
+  String mail;
+  String password;
+  String passwordCheck;
+  String phone;
+
+  UserData(
+    this.name,
+    this.mail,
+    this.password,
+    this.passwordCheck,
+    this.phone,
+  );
+}
