2f03ceee70be1c1671e570c8e7f916367948771d
Astrum93 <jungmin2670@gmail.com>
2024-01-07 22:25:59 +0900

    240107 기본 유저 프로필 배경 이미지 주소 변경

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -2,4 +2,4 @@ const basePath = 'assets';
 const baseProfileImage =
     'https://i.namu.wiki/i/Bge3xnYd4kRe_IKbm2uqxlhQJij2SngwNssjpjaOyOqoRhQlNwLrR2ZiK-JWJ2b99RGcSxDaZ2UCI7fiv4IDDQ.webp';
 const baseProfileBgImage =
-    'https://i.namu.wiki/i/D_XMDDYrR9iE6_h4c9YtSnWz8RqlGqaZ9kyxDRZEkyaLBIes8bYWt0sIrt2lcEOe1bUuILzZMLWEjwvu74yZcY14yCakaUaE96FmF6T7nFmBE3BU1uItvo50XIP_MwKfjcUrPFgtv3oTviwKWJj2MQ.webp';
+    'https://upload.wikimedia.org/wikipedia/commons/2/2f/Hubble_ultra_deep_field.jpg';
