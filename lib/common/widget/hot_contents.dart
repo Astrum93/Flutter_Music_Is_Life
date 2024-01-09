85552d9a3ad4f95f6f76bd64f867bb15ecd7477a
Astrum93 <jungmin2670@gmail.com>
2024-01-09 20:11:45 +0900

    240109 인기 게시물에 관련 회원 정보 출력

--- lib/common/widget/hot_contents.dart
+++ lib/common/widget/hot_contents.dart
@@ -49,138 +49,141 @@ class _HotContentsState extends State<HotContents> {
               // DateTime을 포맷팅
               String formattedDateTime = dateTime.toString();
 
-              return Container(
-                margin: const EdgeInsets.only(bottom: 10),
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.05),
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.4),
-                      blurRadius: 7,
-                    )
-                  ],
-                ),
-                child: Column(
-                  children: [
-                    Column(
-                      children: [
-                        Container(
-                          width: 350,
-                          height: 250,
-                          decoration: BoxDecoration(
-                            border:
-                                Border.all(color: Colors.grey.withOpacity(0.5)),
-                            borderRadius: BorderRadius.circular(16),
-                            image: DecorationImage(
-                              image: NetworkImage(contentsImage),
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          child: const Text(''),
-                        ),
-                        const SizedBox(height: 15),
-                        Text(
-                          title,
-                          style: const TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        Text(
-                          formattedDateTime.substring(0, 10),
-                          style: const TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 15),
-                        Row(
-                          children: [
-                            HashTagTextButton(
-                              onPressed: () {},
-                              text: hashTags[0],
-                            ),
-                            HashTagTextButton(
-                              onPressed: () {},
-                              text: hashTags[1],
-                            ),
-                            HashTagTextButton(
-                              onPressed: () {},
-                              text: hashTags[2],
-                            ),
-                          ],
+              return Stack(
+                children: [
+                  Container(
+                    width: 350,
+                    height: 300,
+                    margin: const EdgeInsets.only(bottom: 10),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(contentsImage),
+                        fit: BoxFit.fill,
+                      ),
+                      color: Colors.grey.withOpacity(0.05),
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          blurRadius: 7,
                         ),
                       ],
                     ),
-                    Row(
-                      //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        StreamBuilder(
-                            stream: userInfoCollection.snapshots(),
-                            builder: (context, snapshot) {
-                              return Row(
-                                children: [
-                                  CircleAvatar(
-                                    radius: 25,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(50),
-                                      child: Image.asset(
-                                          '$basePath/profile/pikachu.png'),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 10),
-                                  const Text(
-                                    'Pikachu',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    '10.3k',
-                                    style: TextStyle(
-                                        color: Colors.grey.shade600,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 13),
-                                  )
-                                ],
-                              );
-                            }),
-                        Row(
-                          children: [
-                            Column(
-                              children: [
-                                IconButton(
+                    child: Text(''),
+                  ),
+
+                  /// 게시물 생성 날짜
+                  Positioned(
+                    right: 10,
+                    bottom: 150,
+                    child: Text(
+                      formattedDateTime.substring(0, 10),
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+
+                  /// 게시물 제목
+                  Positioned(
+                    left: 10,
+                    bottom: 150,
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+
+                  /// 프로필 정보
+                  Positioned(
+                    left: 10,
+                    bottom: 80,
+                    child: StreamBuilder(
+                        stream: userInfoCollection.snapshots(),
+                        builder: (context, snapshot) {
+                          return Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 25,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(50),
+                                  child: Image.asset(
+                                      '$basePath/profile/pikachu.png'),
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              const Text(
+                                'Pikachu',
+                                style: TextStyle(
                                   color: Colors.white,
-                                  iconSize: 25,
-                                  onPressed: () {},
-                                  icon: const Icon(
-                                    Icons.favorite_border_outlined,
-                                    color: Colors.pink,
-                                  ),
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(horizontal: 3),
-                                  child: Text(
-                                    '19650',
-                                    style: TextStyle(
-                                      color: Colors.pinkAccent.shade100,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ],
+                              ),
+                              Text(
+                                '10.3k',
+                                style: TextStyle(
+                                    color: Colors.grey.shade600,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 13),
+                              )
+                            ],
+                          );
+                        }),
+                  ),
+                  Positioned(
+                    right: 10,
+                    bottom: 80,
+                    child: Column(
+                      children: [
+                        IconButton(
+                          color: Colors.white,
+                          iconSize: 25,
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.favorite_border_outlined,
+                            color: Colors.pink,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 3),
+                          child: Text(
+                            '19650',
+                            style: TextStyle(
+                              color: Colors.pinkAccent.shade100,
+                              fontWeight: FontWeight.bold,
                             ),
-                          ],
+                          ),
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+
+                  /// HashTags
+                  Positioned(
+                    bottom: 30,
+                    child: Row(
+                      children: [
+                        HashTagTextButton(
+                          onPressed: () {},
+                          text: hashTags[0],
+                        ),
+                        HashTagTextButton(
+                          onPressed: () {},
+                          text: hashTags[1],
+                        ),
+                        HashTagTextButton(
+                          onPressed: () {},
+                          text: hashTags[2],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               );
             },
             separatorBuilder: (context, index) => const SizedBox(width: 10),
