ceb54b4237dd3e28f8e6466d47ce3754f68f048c
Astrum93 <jungmin2670@gmail.com>
2024-01-17 22:59:00 +0900

    240117 My Page에 Music Search 생성

--- lib/screen/mypage/music/music_search_screen.dart
+++ lib/screen/mypage/music/music_search_screen.dart
@@ -19,7 +19,27 @@ class _MusicSearchScreenState extends State<MusicSearchScreen> {
       body: const Column(
         children: [
           Center(
-            child: SearchMusic(),
+            child: Column(
+              children: [
+                Text(
+                  '음악 검색',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 15),
+                SearchMusic(),
+                SizedBox(height: 15),
+                Text(
+                  '* 해당 검색은 네이버 검색을 통해 이루어 집니다.\n* 올바른 가수명과 노래 제목을 입력해 주세요.\n* 올바르게 검색이 되면 바로 유튜브 영상이 재생됩니다.',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 10,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
           ),
         ],
       ),
