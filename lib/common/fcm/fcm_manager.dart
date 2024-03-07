90633077531f9ea61d92e8db3dc50ef9c20492e1
Astrum93 <jungmin2670@gmail.com>
2024-03-07 23:49:10 +0900

    240307 ForeGround 상태 테스트

--- lib/common/fcm/fcm_manager.dart
+++ lib/common/fcm/fcm_manager.dart
@@ -1,9 +1,24 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
 class FcmManager {
   static void requestPermission() {
     FirebaseMessaging.instance.requestPermission();
   }
 
-  /// BackGround
+  static void initialize() async {
+    /// ForeGround
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      debugPrint('Got a message whilst in the foreground!');
+      debugPrint('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        debugPrint(
+            'Message also contained a notification: ${message.notification}');
+      }
+    });
+
+    /// BackGround
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {});
+  }
 }
