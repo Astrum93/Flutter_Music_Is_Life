468997e26f34b9d7e4677a605e51a85e2d516bf6
Astrum93 <jungmin2670@gmail.com>
2024-03-07 23:23:58 +0900

    240307 FcmManager에 requestPermission 추가

--- lib/common/fcm/fcm_manager.dart
+++ lib/common/fcm/fcm_manager.dart
@@ -1,1 +1,9 @@
-class FcmManager {}
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class FcmManager {
+  static void requestPermission() {
+    FirebaseMessaging.instance.requestPermission();
+  }
+
+  /// BackGround
+}
