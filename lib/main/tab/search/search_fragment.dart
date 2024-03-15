22af771eb2d5a5612876a9c8cb738ccecc50f46c
Astrum93 <jungmin2670@gmail.com>
2024-03-15 22:42:35 +0900

    240315 불필요한 코드 삭제

--- lib/main/tab/search/search_fragment.dart
+++ lib/main/tab/search/search_fragment.dart
@@ -1,6 +1,5 @@
 import 'package:MusicIsLife/main/tab/search/search_app_bar.dart';
 import 'package:MusicIsLife/main/tab/search/search_screen.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SearchFragment extends StatefulWidget {
@@ -12,41 +11,6 @@ class SearchFragment extends StatefulWidget {
 
 class _SearchFragmentState extends State<SearchFragment> {
   final TextEditingController controller = TextEditingController();
-
-  Future<void> getDocument() async {
-    // 참조할 문서 정의하기
-    DocumentReference documentRef =
-        FirebaseFirestore.instance.collection('UserContents').doc('david face');
-
-    // 비동기적으로 데이터 호출하기
-    DocumentSnapshot documentSnapshot = await documentRef.get();
-    if (documentSnapshot.exists) {
-      Map<String, dynamic> data =
-          documentSnapshot.data() as Map<String, dynamic>;
-      print(data);
-      debugPrint(data['hashTags'].toString()); // 문서 데이터 출력
-      debugPrint(data['contents']); // 문서 데이터 출력
-      debugPrint(data['name']); // 문서 데이터 출력
-    } else {
-      print("Document does not exist.");
-    }
-  }
-
-  Future<void> getCollection() async {
-    // 참조할 컬렉션 정의하기
-    CollectionReference collectionRef =
-        FirebaseFirestore.instance.collection('UserContents');
-
-    // 비동기적으로 데이터 호출하기
-    final QuerySnapshot collectionSnapshot = await collectionRef.get();
-
-    // 컬렉션 데이터에 접근
-    for (var doc in collectionSnapshot.docs) {
-      // 각 문서의 데이터에 접근
-      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-      print(data['contents']);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
