47f14aa67fcc6779031168952201d4fe8e86d0b0
Astrum93 <jungmin2670@gmail.com>
2024-03-18 22:29:12 +0900

    240318 FireStore의 UserInfo Collection의 데이터를 가져오는 getUserInfoDataList 생성

--- lib/main/tab/search/data/search_data_manager.dart
+++ lib/main/tab/search/data/search_data_manager.dart
@@ -2,8 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SearchDataManager {
   static Future<List<T>> getUserInfoDataList<T>() async {
-    final userInfoRef = FirebaseFirestore.instance.collection('UserInfo');
-    await userInfoRef.get();
+    final userInfoSnapshot =
+        await FirebaseFirestore.instance.collection('UserInfo').get();
+    final docs = userInfoSnapshot.docs;
+    if (docs.isNotEmpty) {
+      /// docs의 object들을 리스트로 변환해야 함
+    }
     return [];
   }
 }
