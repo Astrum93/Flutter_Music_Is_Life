cbfcc32b70696ed8990ae8b18e18ae9ad3fc5576
Astrum93 <jungmin2670@gmail.com>
2024-03-21 00:18:02 +0900

    240320 각각의 검색 결과 화면 ListView.builder로 수정

--- lib/main/tab/search/search_result_user_info_screen.dart
+++ lib/main/tab/search/search_result_user_info_screen.dart
@@ -1,102 +1,29 @@
-import 'package:MusicIsLife/common/widget/width_height_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SearchResultUserInfo extends StatefulWidget {
-  const SearchResultUserInfo({super.key});
+import 'data/search_data.dart';
 
-  @override
-  State<SearchResultUserInfo> createState() => _SearchResultUserInfoState();
-}
+class SearchResultUserInfo extends StatelessWidget {
+  SearchResultUserInfo({super.key});
 
-class _SearchResultUserInfoState extends State<SearchResultUserInfo> {
+  /// late 키워드를 사용하는 이유는 state생성이 initState보다 빠르기 때문
+  late final searchData = Get.find<SearchData>();
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            '사용자 검색 결과',
-            style: TextStyle(
-              color: Colors.grey,
-              fontWeight: FontWeight.bold,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: 400,
+        child: ListView.builder(
+          itemBuilder: (context, index) => Text(
+            searchData.userInfo[index],
+            style: const TextStyle(
+              color: Colors.white,
             ),
           ),
-          height5,
-          Container(
-            color: Colors.white,
-            width: MediaQuery.of(context).size.width,
-            height: 400,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.amber,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.amber,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.amber,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
