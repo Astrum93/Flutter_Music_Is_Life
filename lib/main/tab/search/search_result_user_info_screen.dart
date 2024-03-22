2aebe5e60da31b8336b37ccff5aaf02d8a060d89
Astrum93 <jungmin2670@gmail.com>
2024-03-22 22:41:48 +0900

    240322 검색 결과 화면에 Image도 같이 보여주기 위한 코드 개선

--- lib/main/tab/search/search_result_user_info_screen.dart
+++ lib/main/tab/search/search_result_user_info_screen.dart
@@ -1,3 +1,5 @@
+import 'package:MusicIsLife/common/widget/width_height_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'data/search_data.dart';
@@ -7,6 +9,8 @@ class SearchResultUserInfo extends StatelessWidget with SearchDataProvider {
 
   @override
   Widget build(BuildContext context) {
+    final userInfoCollection =
+        FirebaseFirestore.instance.collection('UserInfo');
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
       child: SizedBox(
@@ -14,11 +18,17 @@ class SearchResultUserInfo extends StatelessWidget with SearchDataProvider {
         height: 100,
         child: ListView.builder(
           itemCount: searchData.userInfo.length,
-          itemBuilder: (context, index) => Text(
-            searchData.userInfo[index],
-            style: const TextStyle(
-              color: Colors.white,
-            ),
+          itemBuilder: (context, index) => Row(
+            children: [
+              Image.network(''),
+              width10,
+              Text(
+                searchData.userInfo[index],
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
           ),
         ),
       ),
