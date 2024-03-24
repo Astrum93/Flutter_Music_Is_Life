783f09e70caf2d65e1cbc83f000ef8ca77be5c34
Astrum93 <jungmin2670@gmail.com>
2024-03-24 20:45:39 +0900

    240324 SearchDataUtil의 getUserInfoDoc을 바로 DocumentSnapshot의 리스트로 리턴하도록 수정

--- lib/main/tab/search/data/search_data_util.dart
+++ lib/main/tab/search/data/search_data_util.dart
@@ -1,17 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SearchDataUtil {
-  static Future<List> getUserInfoDoc(List idList) async {
+  static Future<List<DocumentSnapshot>> getUserInfoDoc() async {
     final userInfoSnapshot =
         await FirebaseFirestore.instance.collection('UserInfo').get();
     final docs = userInfoSnapshot.docs;
-    if (docs.isNotEmpty) {
-      /// docs의 doc들의 id값을 리스트로 변환해야 함
-      for (var doc in docs) {
-        idList.add(doc.get('userName'));
-      }
-    }
-    return [];
+    return docs;
   }
 
   static Future<List> getContentsDoc(List idList) async {
@@ -21,7 +15,6 @@ class SearchDataUtil {
     if (docs.isNotEmpty) {
       /// docs의 doc들의 id값을 리스트로 변환해야 함
       for (var doc in docs) {
-        //debugPrint(doc.id.toString());
         var docId = doc.id.toString();
         idList.add(docId);
       }
