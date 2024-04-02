4d245914def3c0dd8c55794c4106f3d26f27eea5
Astrum93 <jungmin2670@gmail.com>
2024-04-02 23:49:47 +0900

    240402 이미지 수정 후 carouse_slider 생성

--- lib/main/tab/lounge/lounge_screen.dart
+++ lib/main/tab/lounge/lounge_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class LoungeScreen extends StatefulWidget {
@@ -10,9 +11,13 @@ class LoungeScreen extends StatefulWidget {
 class _LoungeScreenState extends State<LoungeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Column(
-        children: [],
+    return SafeArea(
+      child: CarouselSlider(
+        items: <Widget>[
+          Image.asset('assets/image/planet.png'),
+          Image.asset('assets/image/pink_planet.jpg'),
+        ],
+        options: CarouselOptions(),
       ),
     );
   }
