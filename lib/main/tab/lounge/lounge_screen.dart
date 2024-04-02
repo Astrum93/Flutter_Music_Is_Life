14cd4081ffea16833f8a3647087cd76834e7337c
Astrum93 <jungmin2670@gmail.com>
2024-04-02 23:33:06 +0900

    240402 carousel_slider 패키지 추가

--- lib/main/tab/lounge/lounge_screen.dart
+++ lib/main/tab/lounge/lounge_screen.dart
@@ -7,30 +7,13 @@ class LoungeScreen extends StatefulWidget {
   State<LoungeScreen> createState() => _LoungeScreenState();
 }
 
-class _LoungeScreenState extends State<LoungeScreen>
-    with SingleTickerProviderStateMixin {
-  late final TabController tabController;
-
-  @override
-  void initState() {
-    tabController = TabController(length: 2, vsync: this);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
-
+class _LoungeScreenState extends State<LoungeScreen> {
   @override
   Widget build(BuildContext context) {
-    return TabBar(
-      controller: tabController,
-      tabs: [
-        Placeholder(),
-        Placeholder(),
-      ],
+    return const SafeArea(
+      child: Column(
+        children: [],
+      ),
     );
   }
 }
