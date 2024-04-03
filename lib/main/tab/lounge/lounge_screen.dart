ea12f6481cd7df6ae9f4518858b96fee19fb3101
Astrum93 <jungmin2670@gmail.com>
2024-04-03 23:33:58 +0900

    240403 LoungeSliderIndicator로 Extract Widget

--- lib/main/tab/lounge/lounge_screen.dart
+++ lib/main/tab/lounge/lounge_screen.dart
@@ -31,31 +31,42 @@ class _LoungeScreenState extends State<LoungeScreen> {
                   });
                 }),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: sliderItems.asMap().entries.map((entry) {
-              return GestureDetector(
-                onTap: () => _controller.animateToPage(entry.key),
-                child: Container(
-                  width: 12.0,
-                  height: 12.0,
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 4.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white
-                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
-                ),
-              );
-            }).toList(),
-          ),
-          Container(
-            color: Colors.white,
-            width: MediaQuery.of(context).size.width,
-            height: 200,
-          )
+          LoungeSliderIndicator(controller: _controller, current: _current),
         ],
       ),
+    );
+  }
+}
+
+class LoungeSliderIndicator extends StatelessWidget {
+  final CarouselController _controller;
+  final int _current;
+
+  const LoungeSliderIndicator({
+    super.key,
+    required CarouselController controller,
+    required int current,
+  })  : _controller = controller,
+        _current = current;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: sliderItems.asMap().entries.map((entry) {
+        return GestureDetector(
+          onTap: () => _controller.animateToPage(entry.key),
+          child: Container(
+            width: 12.0,
+            height: 12.0,
+            margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.white
+                    .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+          ),
+        );
+      }).toList(),
     );
   }
 }
