4981931d521c64b1c538ea5efa93cab8af629e28
Astrum93 <jungmin2670@gmail.com>
2024-04-10 21:57:41 +0900

    240410 recommend_friends_screen 생성

---
+++ lib/main/mypage/friends/screen/recommend_friends_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecommendsFriendsScreen extends StatelessWidget {
+  const RecommendsFriendsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
