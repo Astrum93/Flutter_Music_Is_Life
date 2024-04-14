70ce15a21c017b69aea7a9318a53355861197ea4
Astrum93 <jungmin2670@gmail.com>
2024-04-14 21:06:38 +0900

    240414 reusable Widget으로 인한 코드 수정

--- lib/main/mypage/add_friends/screen/recommend_friends_tab_bar_view.dart
+++ lib/main/mypage/add_friends/screen/recommend_friends_tab_bar_view.dart
@@ -1,5 +1,6 @@
 import 'package:MusicIsLife/common/constant/app_colors.dart';
 import 'package:MusicIsLife/common/widget/width_height_widget.dart';
+import 'package:MusicIsLife/main/mypage/add_friends/request_friends_dialog.dart';
 import 'package:MusicIsLife/main/tab/search/data/search_data.dart';
 import 'package:flutter/material.dart';
 
@@ -22,12 +23,30 @@ class RecommendsFriendsTabBarView extends StatelessWidget
             builder: (context) => RequestFriendDialog(
               searchData: searchData,
               index: index,
+              data: searchData.recommendFriends[index].get('userName'),
             ),
           );
         },
         child: Column(
           children: [
-            RecommendFriendProfileWidget(searchData: searchData, index: index),
+            RecommendFriendProfileWidget(
+              searchData: searchData,
+              index: index,
+              borderColor: Colors.transparent,
+              boxColor: (index == 0)
+                  ? Colors.redAccent
+                  : (index == 1)
+                      ? Colors.blue
+                      : (index == 2)
+                          ? Colors.greenAccent
+                          : AppColors.veryDarkGrey,
+            ),
+            height10,
+            Text(
+              searchData.recommendFriends[index].get('userName'),
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
@@ -40,10 +59,14 @@ class RecommendFriendProfileWidget extends StatelessWidget {
     super.key,
     required this.searchData,
     required this.index,
+    required this.boxColor,
+    required this.borderColor,
   });
 
   final SearchData searchData;
   final int index;
+  final Color? boxColor;
+  final Color borderColor;
 
   @override
   Widget build(BuildContext context) {
@@ -52,13 +75,8 @@ class RecommendFriendProfileWidget extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(8.0),
         decoration: BoxDecoration(
-          color: (index == 0)
-              ? Colors.redAccent
-              : (index == 1)
-                  ? Colors.blue
-                  : (index == 2)
-                      ? Colors.greenAccent
-                      : AppColors.veryDarkGrey,
+          color: boxColor,
+          border: Border.all(color: borderColor),
           borderRadius: const BorderRadius.all(
             Radius.circular(30),
           ),
@@ -74,106 +92,6 @@ class RecommendFriendProfileWidget extends StatelessWidget {
               alignment: Alignment.center,
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class RequestFriendDialog extends StatelessWidget {
-  const RequestFriendDialog({
-    super.key,
-    required this.searchData,
-    required this.index,
-  });
-
-  final SearchData searchData;
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      child: Container(
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          color: AppColors.veryDarkGrey,
-          border: Border.all(color: Colors.grey),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(15),
-          ),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          clipBehavior: Clip.none,
-          children: [
-            Positioned(
-              top: -80,
-              child: RecommendFriendProfileWidget(
-                  searchData: searchData, index: index),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                height20,
-                Text(
-                  searchData.recommendFriends[index].get('userName'),
-                  style: const TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.bold),
-                ),
-                height10,
-                const Text(
-                  '친구 요청 하시겠습니까?',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-
-            /// 친구 요청 버튼
-            Positioned(
-              right: 0,
-              bottom: -50,
-              child: Container(
-                width: 40,
-                height: 40,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.veryDarkGrey,
-                    border: Border.all(color: Colors.green)),
-                child: const Center(
-                  child: Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ),
-                ),
-              ),
-            ),
-
-            /// 취소 버튼
-            Positioned(
-              left: 0,
-              bottom: -50,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-                child: Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: AppColors.veryDarkGrey,
-                      border: Border.all(color: Colors.redAccent)),
-                  child: const Center(
-                    child: Icon(
-                      Icons.close,
-                      color: Colors.redAccent,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
         ),
       ),
     );
