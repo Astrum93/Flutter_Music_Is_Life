2eec790061426962d31c74ea8517239cfd351002
Astrum93 <jungmin2670@gmail.com>
2024-04-14 23:18:41 +0900

    240414 FireStore에 데이터 저장 테스트 및 저장 후 Dialog 닫기 및 스낵바 보여주기

--- lib/main/mypage/add_friends/request_friends_dialog.dart
+++ lib/main/mypage/add_friends/request_friends_dialog.dart
@@ -1,10 +1,13 @@
 import 'package:MusicIsLife/common/constant/app_colors.dart';
+import 'package:MusicIsLife/common/widget/button/check_button.dart';
 import 'package:MusicIsLife/common/widget/width_height_widget.dart';
 import 'package:MusicIsLife/main/mypage/add_friends/screen/recommend_friends_tab_bar_view.dart';
 import 'package:MusicIsLife/main/tab/search/data/search_data.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class RequestFriendDialog extends StatelessWidget {
+class RequestFriendDialog extends StatefulWidget {
   const RequestFriendDialog({
     super.key,
     required this.searchData,
@@ -17,27 +20,40 @@ class RequestFriendDialog extends StatelessWidget {
   final String data;
 
   @override
+  State<RequestFriendDialog> createState() => _RequestFriendDialogState();
+}
+
+class _RequestFriendDialogState extends State<RequestFriendDialog> {
+  @override
   Widget build(BuildContext context) {
     return Dialog(
-      child: Container(
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          color: AppColors.veryDarkGrey,
-          border: Border.all(color: Colors.black),
-          borderRadius: const BorderRadius.all(
-            Radius.circular(15),
-          ),
-        ),
+      backgroundColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+      child: SizedBox(
+        width: 300,
+        height: 300,
         child: Stack(
           alignment: Alignment.center,
           clipBehavior: Clip.none,
           children: [
+            Container(
+              width: 300,
+              height: 100,
+              decoration: BoxDecoration(
+                color: AppColors.veryDarkGrey,
+                border: Border.all(color: Colors.black),
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(15),
+                ),
+              ),
+            ),
+
+            /// 프로필 사진
             Positioned(
-              top: -80,
+              top: 20,
               child: RecommendFriendProfileWidget(
-                searchData: searchData,
-                index: index,
+                searchData: widget.searchData,
+                index: widget.index,
                 borderColor: Colors.amberAccent,
                 boxColor: AppColors.veryDarkGrey,
               ),
@@ -47,7 +63,7 @@ class RequestFriendDialog extends StatelessWidget {
               children: [
                 height20,
                 Text(
-                  data,
+                  widget.data,
                   style: const TextStyle(
                       color: Colors.white, fontWeight: FontWeight.bold),
                 ),
@@ -59,48 +75,67 @@ class RequestFriendDialog extends StatelessWidget {
               ],
             ),
 
-            /// 친구 요청 버튼
+            /// 친구 추가 버튼
             Positioned(
               right: 0,
-              bottom: -50,
-              child: Container(
+              bottom: 55,
+              child: CheckButton(
                 width: 40,
                 height: 40,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.veryDarkGrey,
-                    border: Border.all(color: Colors.green)),
-                child: const Center(
-                  child: Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ),
-                ),
+                boxColor: AppColors.veryDarkGrey,
+                borderColor: Colors.greenAccent,
+                icon: Icons.check,
+                iconColor: Colors.greenAccent,
+                onTap: () async {
+                  await FirebaseFirestore.instance
+                      .collection('UserFriends')
+                      .doc(FirebaseAuth.instance.currentUser!.displayName)
+                      .set(
+                    {
+                      'friends': widget
+                          .searchData.recommendFriends[widget.index]
+                          .get('userName')
+                    },
+                  );
+                  if (mounted) {
+                    Navigator.pop(context);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        backgroundColor: Colors.greenAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10.0),
+                            topRight: Radius.circular(10.0),
+                          ),
+                        ),
+                        content: Text(
+                          '친구 추가가 성공적으로 처리 되었습니다 🎉',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.bold),
+                        ),
+                        duration: Duration(seconds: 5),
+                      ),
+                    );
+                  }
+                },
               ),
             ),
 
             /// 취소 버튼
             Positioned(
               left: 0,
-              bottom: -50,
-              child: GestureDetector(
+              bottom: 55,
+              child: CheckButton(
+                width: 40,
+                height: 40,
+                boxColor: AppColors.veryDarkGrey,
+                borderColor: Colors.redAccent,
+                icon: Icons.clear,
+                iconColor: Colors.redAccent,
                 onTap: () {
-                  Navigator.of(context).pop();
+                  Navigator.pop(context);
                 },
-                child: Container(
-                  width: 40,
-                  height: 40,
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: AppColors.veryDarkGrey,
-                      border: Border.all(color: Colors.redAccent)),
-                  child: const Center(
-                    child: Icon(
-                      Icons.close,
-                      color: Colors.redAccent,
-                    ),
-                  ),
-                ),
               ),
             ),
           ],
