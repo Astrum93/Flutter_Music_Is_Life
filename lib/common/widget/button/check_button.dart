3bb3efc39a06333ca7770e5ff7cba50b42b32b76
Astrum93 <jungmin2670@gmail.com>
2024-04-14 21:07:05 +0900

    240414 check_button 생성자에 파라미터 추가

--- lib/common/widget/button/check_button.dart
+++ lib/common/widget/button/check_button.dart
@@ -2,24 +2,29 @@ import 'package:flutter/material.dart';
 
 class CheckButton extends StatelessWidget {
   final void Function() function;
+  final double? width;
+  final double? height;
+  final Color? boxColor;
+  final Color? iconColor;
 
-  const CheckButton(this.function, {super.key});
+  const CheckButton(this.function,
+      {super.key, this.width, this.height, this.boxColor, this.iconColor});
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return GestureDetector(
       onTap: function,
       child: Container(
-        width: 40,
-        height: 40,
-        decoration: const BoxDecoration(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
           shape: BoxShape.circle,
-          color: Colors.white,
+          color: boxColor,
         ),
-        child: const Center(
+        child: Center(
           child: Icon(
             Icons.check,
-            color: Colors.black,
+            color: iconColor,
           ),
         ),
       ),
