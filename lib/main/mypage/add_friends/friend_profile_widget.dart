7545846c9df988dcd67b77b8fda4c60996816840
Astrum93 <jungmin2670@gmail.com>
2024-04-16 19:49:31 +0900

    240416 friend_profile_widget 생성

---
+++ lib/main/mypage/add_friends/friend_profile_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class FriendProfileWidget extends StatelessWidget {
+  const FriendProfileWidget({
+    super.key,
+    required this.searchData,
+    required this.index,
+    required this.boxColor,
+    required this.borderColor,
+  });
+
+  final String searchData;
+  final int index;
+  final Color? boxColor;
+  final Color borderColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: boxColor,
+          border: Border.all(color: borderColor),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(30),
+          ),
+        ),
+        child: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          radius: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(50),
+            child: Image.network(
+              searchData,
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
