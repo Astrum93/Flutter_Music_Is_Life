3d7f20ad633265caf9b1b334949d93ebe79df934
Astrum93 <jungmin2670@gmail.com>
2024-04-18 22:19:46 +0900

    240418 MessengerData 구축 시작

--- lib/main/tab/messenger/data/messenger_data.dart
+++ lib/main/tab/messenger/data/messenger_data.dart
@@ -1,4 +1,5 @@
 import 'package:MusicIsLife/data/memory/firebase/firestore/fire_store_data_util.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 abstract mixin class MessengerProvider {
@@ -10,5 +11,10 @@ class MessengerData extends GetxController {
   void onInit() {
     FireStoreDataUtil.currentUserFriendsDoc();
     super.onInit();
+  }
+
+  void getFriendsUserInfo() async {
+    final DocumentSnapshot currentUserFriendsDoc =
+        await FireStoreDataUtil.currentUserFriendsDoc();
   }
 }
