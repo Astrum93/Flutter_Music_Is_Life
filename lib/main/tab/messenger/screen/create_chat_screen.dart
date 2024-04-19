a3f8ac7ae814eb11e51c316e074e23b9a64c0f8c
Astrum93 <jungmin2670@gmail.com>
2024-04-19 22:46:04 +0900

    240419 MessengerData 종속성 투입 및 GetBuilder를 통해 friends_list_widget build

--- lib/main/tab/messenger/screen/create_chat_screen.dart
+++ lib/main/tab/messenger/screen/create_chat_screen.dart
@@ -1,19 +1,40 @@
 import 'package:MusicIsLife/common/widget/width_height_widget.dart';
 import 'package:MusicIsLife/main/tab/messenger/chat_widget/chat_widget.dart';
+import 'package:MusicIsLife/main/tab/messenger/data/messenger_data.dart';
 import 'package:MusicIsLife/main/tab/messenger/screen/friends_list_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class CreateChatScreen extends StatelessWidget {
+class CreateChatScreen extends StatefulWidget {
   const CreateChatScreen({super.key});
 
   @override
+  State<CreateChatScreen> createState() => _CreateChatScreenState();
+}
+
+class _CreateChatScreenState extends State<CreateChatScreen>
+    with MessengerDataProvider {
+  @override
+  void initState() {
+    Get.put(MessengerData());
+    messengerData.getFriendsUserInfo();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    Get.delete<MessengerData>();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         body: Column(
           children: <Widget>[
             height30,
-            Center(
+            const Center(
               child: Text(
                 '새로운 \n채　팅',
                 style: TextStyle(
@@ -22,15 +43,19 @@ class CreateChatScreen extends StatelessWidget {
                     fontSize: 50),
               ),
             ),
-            Height(60),
+            const Height(60),
 
             /// 채팅방 Widget
-            ChatWidget(),
+            const ChatWidget(),
 
-            Height(60),
+            const Height(60),
 
             /// 친구 목록
-            FriendsListWidget(),
+            GetBuilder<MessengerData>(
+              builder: (messengerData) {
+                return const FriendsListWidget();
+              },
+            ),
           ],
         ),
       ),
