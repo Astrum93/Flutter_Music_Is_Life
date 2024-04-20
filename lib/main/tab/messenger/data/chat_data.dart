8a082b1e90eb107deb107130b4933a1e49f97794
Astrum93 <jungmin2670@gmail.com>
2024-04-20 22:09:36 +0900

    240420 ChatData 생성

---
+++ lib/main/tab/messenger/data/chat_data.dart
@@ -0,0 +1,15 @@
+class ChatData {
+  final String chatImage;
+  final String chatName;
+  final List member;
+  final List likedMember;
+  final int memberCount;
+
+  ChatData(
+    this.chatImage,
+    this.chatName,
+    this.member,
+    this.likedMember,
+    this.memberCount,
+  );
+}
