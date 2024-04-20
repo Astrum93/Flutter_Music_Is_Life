3bbdc22975deb1f01140c32851e1faa6e0332130
Astrum93 <jungmin2670@gmail.com>
2024-04-20 22:10:32 +0900

    240420 ChatData의 member를 활용하여 선택된 친구 구분하기

--- lib/main/tab/messenger/screen/friends_list_widget.dart
+++ lib/main/tab/messenger/screen/friends_list_widget.dart
@@ -1,6 +1,7 @@
 import 'package:MusicIsLife/common/constant/app_colors.dart';
 import 'package:MusicIsLife/common/widget/width_height_widget.dart';
 import 'package:MusicIsLife/main/mypage/add_friends/friend_profile_widget.dart';
+import 'package:MusicIsLife/main/tab/messenger/data/chat_data.dart';
 import 'package:MusicIsLife/main/tab/messenger/data/messenger_data.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -14,6 +15,8 @@ class FriendsListWidget extends StatefulWidget {
 
 class _FriendsListWidgetState extends State<FriendsListWidget>
     with MessengerDataProvider {
+  final ChatData chatData = ChatData('', '', [], [], 0);
+
   @override
   Widget build(BuildContext context) {
     return Obx(
@@ -23,21 +26,50 @@ class _FriendsListWidgetState extends State<FriendsListWidget>
         child: ListView.builder(
           itemCount: messengerData.friendsUserInfoDocs.length,
           scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => Column(
-            children: [
-              FriendProfileWidget(
-                  rxList: messengerData.friendsUserInfoDocs,
-                  index: index,
-                  boxColor: Colors.transparent,
-                  borderColor: AppColors.veryDarkGrey),
-              height10,
-              Text(
-                messengerData.friendsUserInfoDocs[index].get('userName'),
-                style: const TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.bold),
+          itemBuilder: (context, index) {
+            String userName =
+                messengerData.friendsUserInfoDocs[index].get('userName');
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  if (chatData.member.contains(userName)) {
+                    chatData.member.remove(userName);
+                  } else {
+                    chatData.member.add(userName);
+                  }
+                  print(chatData.member);
+                });
+              },
+              child: Column(
+                children: [
+                  FriendProfileWidget(
+                      rxList: messengerData.friendsUserInfoDocs,
+                      index: index,
+                      boxColor: Colors.transparent,
+                      borderColor: chatData.member.contains(userName)
+                          ? Colors.amberAccent
+                          : AppColors.veryDarkGrey),
+                  height10,
+                  Column(
+                    children: [
+                      Text(
+                        userName,
+                        style: const TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      height10,
+                      Icon(
+                        Icons.check_circle_outline_rounded,
+                        color: chatData.member.contains(userName)
+                            ? Colors.amberAccent
+                            : Colors.transparent,
+                      )
+                    ],
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
