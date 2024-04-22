0570b5fe59132a89dd9ad1c539ddc0c779515d97
Astrum93 <jungmin2670@gmail.com>
2024-04-22 19:13:47 +0900

    240422 edit_chat_image 생성

---
+++ lib/main/tab/messenger/chat_widget/edit/edit_chat_image.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+
+import 'package:MusicIsLife/common/constant/app_colors.dart';
+import 'package:MusicIsLife/common/widget/width_height_widget.dart';
+import 'package:MusicIsLife/main/tab/messenger/data/chat_data.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class EditChatImage extends StatefulWidget {
+  const EditChatImage({super.key});
+
+  @override
+  State<EditChatImage> createState() => _EditChatImageState();
+}
+
+class _EditChatImageState extends State<EditChatImage> {
+  // Image 저장 변수
+  File? pickedImage;
+
+  // chatData
+  ChatData chatData = ChatData();
+
+  // Image Picker
+  void _pickerImage() async {
+    final imagePicker = ImagePicker();
+    final pickedImageFile = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 100,
+      maxHeight: 300,
+    );
+    setState(() {
+      if (pickedImageFile != null) {
+        pickedImage = File(pickedImageFile.path);
+      }
+    });
+  }
+
+  // Picked Image 저장
+  void _pickedImageSave() async {
+    // 클라우드 스토리지 버킷에 경로 생성
+    final refImage = FirebaseStorage.instance
+        .ref()
+        .child('userChatImage')
+        .child('${chatData.chatName}_chatImage.png');
+    // 클라우드 스토리지 버킷에 저장
+    await refImage.putFile(pickedImage!);
+
+    // 저장한 이미지 url로 변환
+    final myurl = await refImage.getDownloadURL();
+
+    if (myurl.isNotEmpty) {
+      chatData.chatImage.value = myurl;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      width: 160,
+      height: 250,
+      decoration: BoxDecoration(
+        color: Colors.black,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Column(
+        children: [
+          const Height(10),
+          const Text(
+            '채팅방 이미지 변경',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 30),
+
+          // 프로필 이미지
+          GestureDetector(
+            onTap: () {
+              _pickerImage();
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: AppColors.veryDarkGrey,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: chatData.chatImage.isEmpty
+                  ? const Icon(
+                      Icons.add,
+                      color: Colors.grey,
+                      size: 40,
+                    )
+                  : CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      radius: 40,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(50),
+                        child: Image.network(
+                          chatData.chatImage.toString(),
+                          fit: BoxFit.cover,
+                          alignment: Alignment.center,
+                        ),
+                      ),
+                    ),
+            ),
+          ),
+
+          const Height(20),
+
+          // 닫기 버튼
+          TextButton.icon(
+            onPressed: () {
+              try {
+                if (pickedImage != null) {
+                  _pickedImageSave();
+                }
+                Navigator.of(context).pop();
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text(
+                      '이미지가 선택되지 않았습니다.',
+                      textAlign: TextAlign.center,
+                    ),
+                    backgroundColor: Colors.red,
+                  ),
+                );
+
+                Navigator.pop(context);
+              }
+            },
+            icon: const Icon(Icons.close),
+            label: const Text(
+              'Close',
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
