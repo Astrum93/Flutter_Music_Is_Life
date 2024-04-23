1079a013cbb347d7096cfae10f13cd67bb92bb44
Astrum93 <jungmin2670@gmail.com>
2024-04-23 22:16:36 +0900

    240423 SingleChildScrollView로 감싸 채팅방 이름 수정 팝업창에서 TextFormField를 선택하여 키보드가 올라올 경우 bottom Overflow 현상 해결

--- lib/main/tab/messenger/screen/create_chat_screen.dart
+++ lib/main/tab/messenger/screen/create_chat_screen.dart
@@ -36,76 +36,82 @@ class _CreateChatScreenState extends State<CreateChatScreen>
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: <Widget>[
-            height30,
-            const Center(
-              child: Text(
-                '새로운 \n채　팅',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 50),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              height30,
+              const Center(
+                child: Text(
+                  '새로운 \n채　팅',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50),
+                ),
               ),
-            ),
-            const Height(50),
-            const Text(
-              '* 채팅방 사진과 채팅방 이름을 터치 하여 변경할 수 있습니다.',
-              style: TextStyle(
-                  color: Colors.red, fontWeight: FontWeight.bold, fontSize: 12),
-            ),
-            const Height(20),
-
-            /// 채팅방 Widget
-            GetBuilder<ChatData>(
-              builder: (chatData) {
-                return const ChatWidget();
-              },
-            ),
-
-            const Height(60),
-
-            const Center(
-              child: Text(
-                '친구 목록',
+              const Height(50),
+              const Text(
+                '* 채팅방 사진과 채팅방 이름을 터치 하여 변경할 수 있습니다.',
                 style: TextStyle(
-                    color: Colors.white,
+                    color: Colors.red,
                     fontWeight: FontWeight.bold,
-                    fontSize: 20),
+                    fontSize: 12),
               ),
-            ),
+              const Height(20),
 
-            height20,
-            const Text(
-              '* 채팅방에 추가하고 싶은 친구의 프로필을 선택하여 추가할 수 있습니다.',
-              style: TextStyle(
-                  color: Colors.red, fontWeight: FontWeight.bold, fontSize: 12),
-            ),
-            height20,
-
-            /// 친구 목록
-            GetBuilder<MessengerData>(
-              builder: (messengerData) {
-                return const FriendsListWidget();
-              },
-            ),
-
-            /// 생성 버튼
-            Center(
-              child: CheckButton(
-                width: 50,
-                height: 50,
-                icon: Icons.check,
-                iconColor: Colors.greenAccent,
-                borderColor: AppColors.veryDarkGrey,
-                onTap: () {
-                  print(chatData.chatImage);
-                  print(chatData.member);
-                  print(chatData.chatName);
+              /// 채팅방 Widget
+              GetBuilder<ChatData>(
+                builder: (chatData) {
+                  return const ChatWidget();
                 },
               ),
-            )
-          ],
+
+              const Height(60),
+
+              const Center(
+                child: Text(
+                  '친구 목록',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+
+              height20,
+              const Text(
+                '* 채팅방에 추가하고 싶은 친구의 프로필을 선택하여 추가할 수 있습니다.',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12),
+              ),
+              height20,
+
+              /// 친구 목록
+              GetBuilder<MessengerData>(
+                builder: (messengerData) {
+                  return const FriendsListWidget();
+                },
+              ),
+
+              /// 생성 버튼
+              Center(
+                child: CheckButton(
+                  width: 50,
+                  height: 50,
+                  icon: Icons.check,
+                  iconColor: Colors.greenAccent,
+                  borderColor: AppColors.veryDarkGrey,
+                  onTap: () {
+                    print(chatData.chatImage);
+                    print(chatData.member);
+                    print(chatData.chatName);
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
