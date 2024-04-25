a04bb07326fd9b6f3cb915ef7cf15e1413f45f2a
Astrum93 <jungmin2670@gmail.com>
2024-04-25 20:27:18 +0900

    240425 messenger_data에 있던 friendsUserInfoDocs chat_data로 이동

--- lib/main/tab/messenger/data/chat_data.dart
+++ lib/main/tab/messenger/data/chat_data.dart
@@ -1,3 +1,5 @@
+import 'package:MusicIsLife/data/memory/firebase/firestore/fire_store_data_util.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 abstract mixin class ChatDataProvider {
@@ -10,4 +12,24 @@ class ChatData extends GetxController {
   RxString manager = "".obs;
   RxList member = [].obs;
   RxList likedMember = [].obs;
+  RxList friendsUserInfoDocs = [].obs;
+
+  @override
+  void onInit() {
+    FireStoreDataUtil.currentUserFriendsDoc();
+    super.onInit();
+  }
+
+  void getFriendsUserInfo() async {
+    List friendsId = [];
+    final DocumentSnapshot currentUserFriendsDoc =
+        await FireStoreDataUtil.currentUserFriendsDoc();
+    friendsId = currentUserFriendsDoc.get('friends');
+    for (dynamic id in friendsId) {
+      friendsUserInfoDocs.add(await FirebaseFirestore.instance
+          .collection('UserInfo')
+          .doc(id)
+          .get());
+    }
+  }
 }
--- lib/main/tab/messenger/data/messenger_data.dart
+++ lib/main/tab/messenger/data/messenger_data.dart
@@ -1,5 +1,4 @@
 import 'package:MusicIsLife/data/memory/firebase/firestore/fire_store_data_util.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
 abstract mixin class MessengerDataProvider {
@@ -7,27 +6,12 @@ abstract mixin class MessengerDataProvider {
 }
 
 class MessengerData extends GetxController {
-  RxList friendsUserInfoDocs = [].obs;
   RxList chatRooms = [].obs;
 
   @override
   void onInit() {
-    FireStoreDataUtil.currentUserFriendsDoc();
     FireStoreDataUtil.getUserChats();
     super.onInit();
-  }
-
-  void getFriendsUserInfo() async {
-    List friendsId = [];
-    final DocumentSnapshot currentUserFriendsDoc =
-        await FireStoreDataUtil.currentUserFriendsDoc();
-    friendsId = currentUserFriendsDoc.get('friends');
-    for (dynamic id in friendsId) {
-      friendsUserInfoDocs.add(await FirebaseFirestore.instance
-          .collection('UserInfo')
-          .doc(id)
-          .get());
-    }
   }
 
   void getUserChatRooms() async {
