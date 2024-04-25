fa3eb5f196891a01a81a1b3acad36c7b9246cae1
Astrum93 <jungmin2670@gmail.com>
2024-04-25 19:55:41 +0900

    240425 주석 추가

--- lib/data/memory/firebase/firestore/fire_store_data_util.dart
+++ lib/data/memory/firebase/firestore/fire_store_data_util.dart
@@ -3,11 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
 class FireStoreDataUtil {
-  final userInfoCollection = FirebaseFirestore.instance.collection('UserInfo');
-  final userContentsCollection =
-      FirebaseFirestore.instance.collection('UserContents');
-  final currentUser = FirebaseAuth.instance.currentUser;
-
+  /// 모든 UserInfoCollection Docs
   static Future<List<DocumentSnapshot>> getUserInfoDoc() async {
     final userInfoSnapshot =
         await FirebaseFirestore.instance.collection('UserInfo').get();
@@ -15,6 +11,7 @@ class FireStoreDataUtil {
     return docs;
   }
 
+  /// 모든 UserContents Docs
   static Future<List<DocumentSnapshot>> getContentsDoc() async {
     final userContentsSnapshot =
         await FirebaseFirestore.instance.collection('UserContents').get();
@@ -22,6 +19,7 @@ class FireStoreDataUtil {
     return docs;
   }
 
+  /// 현재 로그인한 UserInfo 컬렉션의 Doc
   static Future<DocumentSnapshot<Map<String, dynamic>>> currentUserDoc() async {
     final userInfo = await FirebaseFirestore.instance
         .collection('UserInfo')
@@ -30,6 +28,7 @@ class FireStoreDataUtil {
     return userInfo;
   }
 
+  /// 현재 로그인한 User의 UserFriends Docs
   static Future<DocumentSnapshot<Map<String, dynamic>>>
       currentUserFriendsDoc() async {
     final userFriends = await FirebaseFirestore.instance
@@ -37,6 +36,17 @@ class FireStoreDataUtil {
         .doc(FirebaseAuth.instance.currentUser!.displayName)
         .get();
     return userFriends;
+  }
+
+  /// 현재 로그인한 User가 포함된 Chats
+  static Future<List<DocumentSnapshot>> getUserChats() async {
+    final userChatSnapshot = await FirebaseFirestore.instance
+        .collection('UserChats')
+        .where('member',
+            arrayContains: FirebaseAuth.instance.currentUser!.displayName)
+        .get();
+    final docs = userChatSnapshot.docs;
+    return docs;
   }
 }
 
