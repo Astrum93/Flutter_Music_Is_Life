0a67a9259d130af7739082bf9a61a5b4252fd815
Astrum93 <jungmin2670@gmail.com>
2024-05-12 23:51:17 +0900

    240512 chat_bubble_widget에 프로필 이미지 주소 및 유저 이름 추가

--- lib/main/tab/messenger/chat_widget/chat_bubble_widget.dart
+++ lib/main/tab/messenger/chat_widget/chat_bubble_widget.dart
@@ -3,15 +3,20 @@ import 'package:flutter_chat_bubble/chat_bubble.dart';
 
 class ChatBubbleWidget extends StatelessWidget {
   final String message;
+  final String profileImage;
+  final String userName;
   final bool isMe;
 
-  const ChatBubbleWidget(this.message, this.isMe, {super.key});
+  const ChatBubbleWidget(
+      this.message, this.isMe, this.userName, this.profileImage,
+      {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
+        /// 보내는 사람이 본인인 경우
         if (isMe)
           Padding(
             padding: const EdgeInsets.only(right: 5),
@@ -20,17 +25,35 @@ class ChatBubbleWidget extends StatelessWidget {
               alignment: Alignment.topRight,
               margin: const EdgeInsets.only(top: 20),
               backGroundColor: Colors.blue,
-              child: Container(
-                constraints: BoxConstraints(
-                  maxWidth: MediaQuery.of(context).size.width * 0.7,
-                ),
-                child: Text(
-                  message,
-                  style: const TextStyle(color: Colors.white),
-                ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    radius: 40,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.network(
+                        profileImage,
+                        fit: BoxFit.cover,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * 0.7,
+                    ),
+                    child: Text(
+                      message,
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
+
+        /// 보내는 사람이 본인이 아닌 경우
         if (!isMe)
           Padding(
             padding: const EdgeInsets.only(left: 5),
@@ -38,14 +61,30 @@ class ChatBubbleWidget extends StatelessWidget {
               clipper: ChatBubbleClipper6(type: BubbleType.receiverBubble),
               backGroundColor: const Color(0xffE7E7ED),
               margin: const EdgeInsets.only(top: 20),
-              child: Container(
-                constraints: BoxConstraints(
-                  maxWidth: MediaQuery.of(context).size.width * 0.7,
-                ),
-                child: Text(
-                  message,
-                  style: const TextStyle(color: Colors.black),
-                ),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.transparent,
+                    radius: 40,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.network(
+                        profileImage,
+                        fit: BoxFit.cover,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * 0.7,
+                    ),
+                    child: Text(
+                      message,
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
               ),
             ),
           )
