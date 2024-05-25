0c41e85a7693f506b82875728aa2bd37dbc69661
Astrum93 <jungmin2670@gmail.com>
2024-05-25 22:18:46 +0900

    240525 뮤직 리스트 뻐대 구성 완료

--- lib/main/mypage/music/music_collection/music_collection_screen.dart
+++ lib/main/mypage/music/music_collection/music_collection_screen.dart
@@ -1,4 +1,6 @@
 import 'package:MusicIsLife/common/constant/app_colors.dart';
+import 'package:MusicIsLife/common/constants.dart';
+import 'package:MusicIsLife/common/widget/width_height_widget.dart';
 import 'package:flutter/material.dart';
 
 class MusicCollectionScreen extends StatefulWidget {
@@ -16,6 +18,230 @@ class _MusicCollectionScreenState extends State<MusicCollectionScreen> {
         appBar: AppBar(
           title: const Text('뮤직 리스트'),
           centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: MediaQuery.of(context).size.width - 10,
+                  height: 80,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 30,
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                          child: Image.network(baseProfileImage),
+                        ),
+                      ),
+                      const Text(
+                        '음악 제목',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        '가수 이름',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                height10,
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: MediaQuery.of(context).size.width - 10,
+                  height: 80,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 30,
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                          child: Image.network(baseProfileImage),
+                        ),
+                      ),
+                      const Text(
+                        '음악 제목',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        '가수 이름',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                height10,
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: MediaQuery.of(context).size.width - 10,
+                  height: 80,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 30,
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                          child: Image.network(baseProfileImage),
+                        ),
+                      ),
+                      const Text(
+                        '음악 제목',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        '가수 이름',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                height10,
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: MediaQuery.of(context).size.width - 10,
+                  height: 80,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 30,
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                          child: Image.network(baseProfileImage),
+                        ),
+                      ),
+                      const Text(
+                        '음악 제목',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        '가수 이름',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                height10,
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: MediaQuery.of(context).size.width - 10,
+                  height: 80,
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        radius: 30,
+                        child: ClipRRect(
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                          child: Image.network(baseProfileImage),
+                        ),
+                      ),
+                      const Text(
+                        '음악 제목',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Text(
+                        '가수 이름',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                height10,
+              ],
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
