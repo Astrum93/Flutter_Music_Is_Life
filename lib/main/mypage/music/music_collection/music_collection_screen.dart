d4f880280bb7f2314dbbaee269b33fa8b93f50c2
Astrum93 <jungmin2670@gmail.com>
2024-05-25 22:05:43 +0900

    240525 scaffold로 뼈대 구성

--- lib/main/mypage/music/music_collection/music_collection_screen.dart
+++ lib/main/mypage/music/music_collection/music_collection_screen.dart
@@ -1,3 +1,4 @@
+import 'package:MusicIsLife/common/constant/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class MusicCollectionScreen extends StatefulWidget {
@@ -10,6 +11,23 @@ class MusicCollectionScreen extends StatefulWidget {
 class _MusicCollectionScreenState extends State<MusicCollectionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('뮤직 리스트'),
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: AppColors.veryDarkGrey,
+          splashColor: Colors.amberAccent.withOpacity(0.5),
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ),
+    );
   }
 }
