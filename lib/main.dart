36b1298117fedec0d0d9d0425b822b0aec47afa6
Astrum93 <jungmin2670@gmail.com>
2024-05-27 23:38:20 +0900

    240527 .env 파일을 dotenv가 못 찾아서 몇 가지 테스트 구문 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:MusicIsLife/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -5,12 +7,21 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'main/naspace.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  debugPrint('Current directory: ${Directory.current.path}');
+
   await dotenv.load(fileName: ".env");
+
+  if (File('.env').existsSync()) {
+    debugPrint('.env file found');
+  } else {
+    debugPrint('.env file not found');
+  }
   runApp(
     const NAspace(),
   );
