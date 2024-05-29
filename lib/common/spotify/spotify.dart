dd46ec1dc8cdf9fe10968ec2f17567a4925709c1
Astrum93 <jungmin2670@gmail.com>
2024-05-29 21:35:02 +0900

    240529 spotify 인증 절차 뒤 토큰 받는 getAccessToken 메서드 생성

--- lib/common/spotify/spotify.dart
+++ lib/common/spotify/spotify.dart
@@ -1,8 +1,31 @@
+import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 import 'package:spotify_sdk/spotify_sdk.dart';
 
 class Spotify {
   final String clientId = dotenv.env['CLIENT_ID'] ?? '';
+  final String clientSecret = dotenv.env['CLIENT_SECRET'] ?? '';
+
+  Future<String> getAccessToken() async {
+    final response = await http.post(
+      Uri.parse('https://accounts.spotify.com/api/token'),
+      headers: {
+        'Authorization':
+            'Basic ${base64Encode(utf8.encode('$clientId:$clientSecret'))}',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: {'grant_type': 'client_credentials'},
+    );
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      return json['access_token'];
+    } else {
+      throw Exception('Failed to get access token');
+    }
+  }
 
   playSong() async {
     var res = await SpotifySdk.connectToSpotifyRemote(
