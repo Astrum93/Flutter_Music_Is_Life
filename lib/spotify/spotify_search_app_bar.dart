12f438ee8923d34f65b6449555a30863334774bc
Astrum93 <jungmin2670@gmail.com>
2024-06-02 14:47:56 +0900

    240602 spotify_search_app_bar 생성

---
+++ lib/spotify/spotify_search_app_bar.dart
@@ -0,0 +1,61 @@
+import 'package:MusicIsLife/main/mypage/music/music_collection/music_collection_screen.dart';
+import 'package:flutter/material.dart';
+
+class SpotifySearchAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  final TextEditingController? controller;
+
+  const SpotifySearchAppBar({this.controller, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SizedBox(
+          height: 56,
+          child: Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const MusicCollectionScreen()));
+                },
+                child: const Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.grey,
+                ),
+              ),
+              Expanded(
+                child: TextFormField(
+                  controller: controller,
+                  textInputAction: TextInputAction.search,
+                  cursorColor: Colors.grey,
+                  keyboardType: TextInputType.text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: const InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.amber)),
+                    hintText: '노래 제목을 입력 해주세요.',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    contentPadding: EdgeInsets.all(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size(double.infinity, 56.0);
+}
