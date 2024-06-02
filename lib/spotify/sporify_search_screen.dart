2c3a06a24012d708fff738b8a6bced94132eb662
Astrum93 <jungmin2670@gmail.com>
2024-06-02 14:52:26 +0900

    240602 spotify_search_screen에 기본적인 구조 추가

--- lib/spotify/sporify_search_screen.dart
+++ lib/spotify/sporify_search_screen.dart
@@ -1,3 +1,5 @@
+import 'package:MusicIsLife/common/constant/constants.dart';
+import 'package:MusicIsLife/spotify/spotify_search_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class SpotifySearchScreen extends StatefulWidget {
@@ -10,6 +12,49 @@ class SpotifySearchScreen extends StatefulWidget {
 class _SpotifySearchScreenState extends State<SpotifySearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            SpotifySearchAppBar(),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      radius: 30,
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                        child: Image.network(baseProfileImage),
+                      ),
+                    ),
+                    const Text(
+                      '음악 제목',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Text(
+                      '가수 이름',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
