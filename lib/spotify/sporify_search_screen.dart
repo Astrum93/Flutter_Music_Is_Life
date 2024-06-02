274c74718a5839eee1cee563384df686613f3745
Astrum93 <jungmin2670@gmail.com>
2024-06-02 15:01:49 +0900

    240602 ListView.builder로 변경

--- lib/spotify/sporify_search_screen.dart
+++ lib/spotify/sporify_search_screen.dart
@@ -1,5 +1,7 @@
 import 'package:MusicIsLife/common/constant/constants.dart';
+import 'package:MusicIsLife/common/widget/width_height_widget.dart';
 import 'package:MusicIsLife/spotify/spotify_search_app_bar.dart';
+import 'package:MusicIsLife/spotify/spotify_web_api_service.dart';
 import 'package:flutter/material.dart';
 
 class SpotifySearchScreen extends StatefulWidget {
@@ -10,6 +12,8 @@ class SpotifySearchScreen extends StatefulWidget {
 }
 
 class _SpotifySearchScreenState extends State<SpotifySearchScreen> {
+  final SpotifyWebApiService spotifyWebApiService = SpotifyWebApiService();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,40 +21,54 @@ class _SpotifySearchScreenState extends State<SpotifySearchScreen> {
         body: Column(
           children: [
             SpotifySearchAppBar(),
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Expanded(
+              child: ListView.builder(
+                itemCount: 5,
+                itemBuilder: (context, index) => Column(
                   children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.transparent,
-                      radius: 30,
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                        child: Image.network(baseProfileImage),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      width: MediaQuery.of(context).size.width - 10,
+                      height: 80,
+                      decoration: const BoxDecoration(
+                        color: Colors.transparent,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.transparent,
+                            radius: 30,
+                            child: ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(30),
+                              ),
+                              child: Image.network(baseProfileImage),
+                            ),
+                          ),
+                          const Text(
+                            '음악 제목',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const Text(
+                            '가수 이름',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const Text(
-                      '음악 제목',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const Text(
-                      '가수 이름',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
+                    height10,
                   ],
                 ),
-              ],
+              ),
             ),
           ],
         ),
