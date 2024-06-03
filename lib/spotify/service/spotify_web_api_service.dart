4e144472b599e968b7e622abcf7d8c9ae2ff8b8c
Astrum93 <jungmin2670@gmail.com>
2024-06-03 21:17:46 +0900

    240603 spotify_search_data와 spotify_web_api_service 분리

--- lib/spotify/service/spotify_web_api_service.dart
+++ lib/spotify/service/spotify_web_api_service.dart
@@ -1,20 +1,11 @@
 import 'dart:convert';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
-import 'package:spotify_sdk/spotify_sdk.dart';
 
-abstract mixin class SpotifyWebApiServiceDataProvider {
-  /// late 키워드를 사용하는 이유는 state생성이 initState보다 빠르기 때문
-  late final spotifyData = Get.find<SpotifyWebApiService>();
-}
-
-class SpotifyWebApiService extends GetxController {
+abstract mixin class SpotifyWebApiService {
   final String clientId = dotenv.env['CLIENT_ID'] ?? '';
   final String clientSecret = dotenv.env['CLIENT_SECRET'] ?? '';
-  final RxList searchResult = [].obs;
 
   Future<String> getAccessToken() async {
     final response = await http.post(
@@ -33,58 +24,5 @@ class SpotifyWebApiService extends GetxController {
     } else {
       throw Exception('Failed to get access token');
     }
-  }
-
-  Future<RxList> searchMusic(String query) async {
-    final token = await getAccessToken();
-    final response = await http.get(
-      Uri.parse('https://api.spotify.com/v1/search?q=$query&type=track'),
-      headers: {
-        'Authorization': 'Bearer $token',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      final json = jsonDecode(response.body);
-
-      for (var item in json['tracks']['items']) {
-        final String trackImage = item['album']['images'][0]['url'];
-        final String trackName = item['name'];
-        final String artistsName = item['artists'][0]['name'];
-
-        Map<dynamic, dynamic> trackInfo = {
-          'trackImage': trackImage,
-          'trackName': trackName,
-          'artistsName': artistsName,
-        };
-
-        searchResult.add(trackInfo);
-      }
-      debugPrint(searchResult.toString());
-
-      return searchResult;
-    } else {
-      throw Exception('Failed to search tracks');
-    }
-  }
-
-  /// 스포티파이 음악 검색 AppBar
-  void spotifySearchMusic(String keyword) async {
-    if (keyword.isEmpty) {
-      searchResult.clear();
-      return;
-    }
-    await searchMusic(keyword);
-  }
-
-  /// 기기에 Spotify 앱이 설치 되어 있어야 사용 가능
-  playSong(String trackId) async {
-    var res = await SpotifySdk.connectToSpotifyRemote(
-        clientId: clientId,
-        redirectUrl: "YOUR_APP_NAME://",
-        scope:
-            "app-remote-control,user-modify-playback-state,playlist-read-private");
-    debugPrint(res.toString());
-    SpotifySdk.play(spotifyUri: "spotify:track:$trackId");
   }
 }
