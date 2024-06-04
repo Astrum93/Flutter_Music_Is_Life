46725614e0c1cd83c389f9b201d6cd76b2fda3a4
Astrum93 <jungmin2670@gmail.com>
2024-06-04 22:51:19 +0900

    240604 music_collection_screen에 HomeDataProvider 상속 및 GetX를 활용한 종속성 주입

--- lib/main/mypage/music/music_collection/music_collection_screen.dart
+++ lib/main/mypage/music/music_collection/music_collection_screen.dart
@@ -1,8 +1,11 @@
 import 'package:MusicIsLife/common/constant/app_colors.dart';
 import 'package:MusicIsLife/common/constant/constants.dart';
 import 'package:MusicIsLife/common/widget/width_height_widget.dart';
+import 'package:MusicIsLife/data/memory/firebase/firebase_auth/firebase_auth_user.dart';
+import 'package:MusicIsLife/main/tab/home/data/home_data.dart';
 import 'package:MusicIsLife/spotify/spotify_search_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MusicCollectionScreen extends StatefulWidget {
   const MusicCollectionScreen({super.key});
@@ -11,7 +14,21 @@ class MusicCollectionScreen extends StatefulWidget {
   State<MusicCollectionScreen> createState() => _MusicCollectionScreenState();
 }
 
-class _MusicCollectionScreenState extends State<MusicCollectionScreen> {
+class _MusicCollectionScreenState extends State<MusicCollectionScreen>
+    with FirebaseAuthUser, HomeDataProvider {
+  @override
+  void initState() {
+    super.initState();
+    Get.put(HomeData());
+    homeData.userPlayListCreate();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    Get.delete<HomeData>();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -20,53 +37,68 @@ class _MusicCollectionScreenState extends State<MusicCollectionScreen> {
           title: const Text('뮤직 리스트'),
           centerTitle: true,
         ),
-        body: ListView(
-          children: [
-            Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  width: MediaQuery.of(context).size.width - 10,
-                  height: 80,
-                  decoration: const BoxDecoration(
-                    color: Colors.transparent,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        radius: 30,
-                        child: ClipRRect(
+        body: Obx(
+          () => ListView.builder(
+            itemCount: homeData.userPlayList.length,
+            itemBuilder: (context, index) {
+              var track = homeData.userPlayList[index];
+              var trackImage = track['trackImage'] ?? baseProfileImage;
+              var trackName = track['trackName'] ?? '';
+              var artistsName = track['artistsName'] ?? '';
+              return Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: MediaQuery.of(context).size.width - 10,
+                    height: 150,
+                    decoration: const BoxDecoration(
+                      color: Colors.transparent,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        ClipRRect(
                           borderRadius: const BorderRadius.all(
                             Radius.circular(30),
                           ),
-                          child: Image.network(baseProfileImage),
+                          child: Image.network(trackImage),
                         ),
-                      ),
-                      const Text(
-                        '음악 제목',
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                        width30,
+                        Expanded(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Text(
+                                trackName,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text(
+                                artistsName,
+                                style: const TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      const Text(
-                        '가수 이름',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                height10,
-              ],
-            ),
-          ],
+                  height10,
+                ],
+              );
+            },
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
