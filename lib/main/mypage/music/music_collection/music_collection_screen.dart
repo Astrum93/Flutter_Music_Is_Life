cdface56f98f172123ad38a91f4884372fc5ad77
Astrum93 <jungmin2670@gmail.com>
2024-06-06 23:18:35 +0900

    240606 bottom Padding 추가

--- lib/main/mypage/music/music_collection/music_collection_screen.dart
+++ lib/main/mypage/music/music_collection/music_collection_screen.dart
@@ -25,89 +25,92 @@ class _MusicCollectionScreenState extends State<MusicCollectionScreen>
           title: const Text('뮤직 리스트'),
           centerTitle: true,
         ),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance
-              .collection('UserInfo')
-              .doc(FirebaseAuth.instance.currentUser!.displayName)
-              .collection('UserPlayList')
-              .snapshots(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+        body: Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('UserInfo')
+                .doc(FirebaseAuth.instance.currentUser!.displayName)
+                .collection('UserPlayList')
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
 
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            }
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
+                );
+              }
 
-            final List<DocumentSnapshot<Map<String, dynamic>>> collectionDocs =
-                snapshot.data!.docs;
+              final List<DocumentSnapshot<Map<String, dynamic>>>
+                  collectionDocs = snapshot.data!.docs;
 
-            return ListView.builder(
-                itemCount: collectionDocs.length,
-                itemBuilder: (context, index) {
-                  var track = collectionDocs[index];
-                  var trackImage = track['trackImage'] ?? baseProfileImage;
-                  var trackName = track['trackName'] ?? '';
-                  var artistsName = track['artistsName'] ?? '';
-                  return Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        width: MediaQuery.of(context).size.width - 10,
-                        height: 150,
-                        decoration: const BoxDecoration(
-                          color: Colors.transparent,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            ClipRRect(
-                              borderRadius: const BorderRadius.all(
-                                Radius.circular(30),
+              return ListView.builder(
+                  itemCount: collectionDocs.length,
+                  itemBuilder: (context, index) {
+                    var track = collectionDocs[index];
+                    var trackImage = track['trackImage'] ?? baseProfileImage;
+                    var trackName = track['trackName'] ?? '';
+                    var artistsName = track['artistsName'] ?? '';
+                    return Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          width: MediaQuery.of(context).size.width - 10,
+                          height: 150,
+                          decoration: const BoxDecoration(
+                            color: Colors.transparent,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              ClipRRect(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(30),
+                                ),
+                                child: Image.network(trackImage),
                               ),
-                              child: Image.network(trackImage),
-                            ),
-                            width30,
-                            Expanded(
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    trackName,
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
+                              width30,
+                              Expanded(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      trackName,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
                                     ),
-                                    maxLines: 1,
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  Text(
-                                    artistsName,
-                                    style: const TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.grey,
+                                    Text(
+                                      artistsName,
+                                      style: const TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.grey,
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      height10,
-                    ],
-                  );
-                });
-          },
+                        height10,
+                      ],
+                    );
+                  });
+            },
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
