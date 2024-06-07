74af8bc5ef992eb226baa898f56048d54605c4e3
Astrum93 <jungmin2670@gmail.com>
2024-06-07 19:04:50 +0900

    240607 flutter_native_splash 적용 완료

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:MusicIsLife/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'main/naspace.dart';
 
@@ -11,8 +12,11 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await dotenv.load(fileName: "assets/config/.env");
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   runApp(
     const NAspace(),
   );
+  FlutterNativeSplash.remove();
 }
