62b6b351b748c974cbe02698d7e11ab0c51e9ffd
Astrum93 <jungmin2670@gmail.com>
2024-06-13 23:21:52 +0900

    240613 music_collection_screen 노래 제목 및 가수 이름이 너무 길면 애니메이션 효과 적용

--- lib/main/mypage/music/music_collection/music_collection_screen.dart
+++ lib/main/mypage/music/music_collection/music_collection_screen.dart
@@ -7,6 +7,7 @@ import 'package:MusicIsLife/spotify/spotify_search_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 
 class MusicCollectionScreen extends StatefulWidget {
   const MusicCollectionScreen({super.key});
@@ -111,29 +112,68 @@ class _MusicCollectionScreenState extends State<MusicCollectionScreen>
                                 ),
                                 width30,
                                 Expanded(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: [
-                                      Text(
-                                        trackName,
-                                        style: const TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                        ),
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                      Text(
-                                        artistsName,
-                                        style: const TextStyle(
-                                          fontSize: 15,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                    ],
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 8),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.end,
+                                      children: [
+                                        trackName.length > 20
+                                            ? SizedBox(
+                                                width: trackName.length *
+                                                    11.toDouble(),
+                                                height: 50,
+                                                child: Marquee(
+                                                  text: trackName,
+                                                  style: const TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 20,
+                                                    color: Colors.white,
+                                                  ),
+                                                  scrollAxis: Axis.horizontal,
+                                                  velocity: 30,
+                                                  blankSpace: 40,
+                                                ),
+                                              )
+                                            : Text(
+                                                trackName,
+                                                style: const TextStyle(
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.white,
+                                                ),
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                        artistsName.length > 15
+                                            ? SizedBox(
+                                                width: artistsName.length *
+                                                    11.toDouble(),
+                                                height: 50,
+                                                child: Marquee(
+                                                  text: artistsName,
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 15,
+                                                    color: Colors.grey.shade600,
+                                                  ),
+                                                  scrollAxis: Axis.horizontal,
+                                                  velocity: 30,
+                                                  blankSpace: 40,
+                                                ),
+                                              )
+                                            : Text(
+                                                artistsName,
+                                                style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.grey.shade600,
+                                                ),
+                                              ),
+                                      ],
+                                    ),
                                   ),
                                 ),
                               ],
