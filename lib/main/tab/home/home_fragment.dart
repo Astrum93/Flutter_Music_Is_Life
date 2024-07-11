c86b3aa921d77975b13afbf72df7664361f4e7f0
Astrum93 <jungmin2670@gmail.com>
2024-07-11 22:55:12 +0900

    240711 PopScope를 사용하여 로그아웃 확인 용 Dialog 생성

--- lib/main/tab/home/home_fragment.dart
+++ lib/main/tab/home/home_fragment.dart
@@ -1,3 +1,4 @@
+import 'package:MusicIsLife/common/constant/app_colors.dart';
 import 'package:MusicIsLife/common/fcm/fcm_manager.dart';
 import 'package:MusicIsLife/main/tab/home/screen/home_screen.dart';
 import 'package:MusicIsLife/main/tab/lounge/lounge_fragment.dart';
@@ -31,58 +32,94 @@ class _HomeFragmentState extends State<HomeFragment> with FirebaseAuthUser {
   @override
   void initState() {
     super.initState();
-
     FcmManager.requestPermission();
     FcmManager.initialize();
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Scaffold(
-            body: SafeArea(
-              child: _tabItems.elementAt(_currentIndex),
-            ),
-
-            /// Bottom Navigation Bar
-            bottomNavigationBar: BottomNavigationBar(
-              currentIndex: _currentIndex,
-              type: BottomNavigationBarType.fixed,
-              selectedItemColor: Colors.white,
-              unselectedItemColor: Colors.grey.shade600,
-              showUnselectedLabels: true,
-              backgroundColor: Colors.black,
-              onTap: (int index) {
-                setState(() {
-                  _currentIndex = index;
-                });
-              },
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home_rounded),
-                  label: '홈',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.search_rounded),
-                  label: '검색',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.multitrack_audio_sharp),
-                  label: '라운지',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.messenger_outline_rounded),
-                  label: '메신저',
-                ),
-              ],
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (bool didPop) {
+        if (!didPop) {
+          showDialog(
+            context: context,
+            builder: (context) => Material(
+              color: Colors.transparent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 300,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                      border: Border.all(color: Colors.redAccent),
+                      color: AppColors.veryDarkGrey,
+                    ),
+                    child: const Center(
+                      child: Text(
+                        '현제 계정에서 \n로그아웃 하시겠습니까?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         }
-        return const WelcomeScreen();
       },
+      child: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Scaffold(
+              body: SafeArea(
+                child: _tabItems.elementAt(_currentIndex),
+              ),
+
+              /// Bottom Navigation Bar
+              bottomNavigationBar: BottomNavigationBar(
+                currentIndex: _currentIndex,
+                type: BottomNavigationBarType.fixed,
+                selectedItemColor: Colors.white,
+                unselectedItemColor: Colors.grey.shade600,
+                showUnselectedLabels: true,
+                backgroundColor: Colors.black,
+                onTap: (int index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home_rounded),
+                    label: '홈',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.search_rounded),
+                    label: '검색',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.multitrack_audio_sharp),
+                    label: '라운지',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.messenger_outline_rounded),
+                    label: '메신저',
+                  ),
+                ],
+              ),
+            );
+          }
+          return const WelcomeScreen();
+        },
+      ),
     );
   }
 }
