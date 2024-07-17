b1cea3255878ff25fee0d425437e1628cd08c642
Astrum93 <jungmin2670@gmail.com>
2024-07-17 22:35:10 +0900

    240717 follower_tab_bar_view, following_tab_bar_view 생성

---
+++ lib/main/mypage/favorites/widget/follower_tab_bar_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FollowerTabBarView extends StatefulWidget {
+  const FollowerTabBarView({super.key});
+
+  @override
+  State<FollowerTabBarView> createState() => _FollowerTabBarViewState();
+}
+
+class _FollowerTabBarViewState extends State<FollowerTabBarView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main/mypage/favorites/widget/following_tab_bar_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FollowingTabBarView extends StatefulWidget {
+  const FollowingTabBarView({super.key});
+
+  @override
+  State<FollowingTabBarView> createState() => _FollowingTabBarViewState();
+}
+
+class _FollowingTabBarViewState extends State<FollowingTabBarView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
