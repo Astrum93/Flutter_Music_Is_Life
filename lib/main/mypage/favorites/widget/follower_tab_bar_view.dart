198be02a605502c5c1264b730f1e51cce10dbdb8
Astrum93 <jungmin2670@gmail.com>
2024-07-25 21:21:13 +0900

    240725 팔로워 UI 구현 완료

--- lib/main/mypage/favorites/widget/follower_tab_bar_view.dart
+++ lib/main/mypage/favorites/widget/follower_tab_bar_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:music_is_life/data/memory/firebase/firebase_auth/firebase_auth_user.dart';
 import 'package:music_is_life/data/memory/firebase/firestore/firebase_collection_reference.dart';
@@ -27,46 +28,68 @@ class _FollowerTabBarViewState extends State<FollowerTabBarView>
         return ListView.builder(
           itemCount: follower.length,
           itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      padding: const EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        color: Colors.transparent,
-                        border: Border.all(color: Colors.amberAccent),
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                      ),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.transparent,
-                        radius: 40,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(50),
+            String userId = follower[index];
 
-                          // Image.network(
-                          //   ,
-                          //   fit: BoxFit.cover,
-                          //   alignment: Alignment.center,
-                          // ),
+            return FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('UserInfo')
+                  .doc(userId)
+                  .get(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const SizedBox();
+                }
+                if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                }
+                if (snapshot.hasData) {
+                  String userName = snapshot.data!.get('userName');
+                  String userProfileImage =
+                      snapshot.data!.get('userProfileImage');
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(color: Colors.amberAccent),
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(30),
+                              ),
+                            ),
+                            child: CircleAvatar(
+                              backgroundColor: Colors.transparent,
+                              radius: 40,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(50),
+                                child: Image.network(
+                                  userProfileImage,
+                                  fit: BoxFit.cover,
+                                  alignment: Alignment.center,
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
+                        Text(
+                          userName,
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                      ],
                     ),
-                  ),
-                  Text(
-                    'userName',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
-                  ),
-                ],
-              ),
+                  );
+                } else {
+                  return Text('error');
+                }
+              },
             );
           },
         );
